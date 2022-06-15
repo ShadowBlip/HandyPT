0e2d0f8c3b6ffda5cc58e765d79ed255b5dd9f6d
Derek J. Clark <derekjohn.clark@gmail.com>
2022-06-14 23:08:47 -0700

    push this so billy can figure it out.

--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,24 @@ export const load = (smm: SMM) => {
   console.info(`${TITLE} plugin loaded!`);
 
   const pt = new PowerTools(smm);
-
+  const system_id = pt.getVersion();
+  console.info(`${system_id} found`);
+;
   // Add quickaccess menu
   smm.InGameMenu.addMenuItem({
     id: PLUGIN_ID,
     title: TITLE,
     render: async (smm: SMM, root: HTMLElement) =>
-      render(<App smm={smm} tools={pt} />, root),
+      render(<App smm={smm} pt={pt} />, root),
   });
+  //system_label!.innerText = system_id 
 };
 
+export const onLoad = () => {
+  const system_label = document.getElementById("sysID_Lab") as HTMLInputElement | null;
+ console.info(`${JSON.stringify(document)} found`);
+  
+  }
 export const unload = (smm: SMM) => {
   console.info(`${TITLE} plugin unloaded!`);
   smm.InGameMenu.removeMenuItem(PLUGIN_ID);
