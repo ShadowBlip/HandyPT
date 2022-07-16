5c5bc804caa7c33cdde9d1bca9d603aa25907566
Derek J. Clark <derekjohn.clark@gmail.com>
2022-07-15 22:27:16 -0700

    Move level logic to Util. Lined everything up. Moved functional utilities to top, info to bottom.

--- src/components/Battery.tsx
+++ src/components/Battery.tsx
@@ -4,25 +4,11 @@ export interface LabelProperties {
   children?: ComponentChildren;
 }
 
-
 // Battery shit
 export class Battery extends Component<AppProps> {
   timer: number = 0;
   capacityRef = createRef();
   powerDrawRef = createRef();
-
-  // gets the current power by device
-  async getPower(device: string): Promise<number> {
-    try {
-      const output = await this.props.smm.FS.readFile(
-        `/sys/class/power_supply/BAT0/${device}`
-      );
-      return parseInt(output.trim());
-    } catch (err) {
-      console.log(`Error fetching charge: ${err}`);
-      return 0;
-    }
-  }
 
   // update will be called every second, refreshing the battery capacity and
   // power draw.
@@ -30,9 +16,9 @@ export class Battery extends Component<AppProps> {
     //console.log('Updating battery stats');
     const batCapacityNow: HTMLDivElement = this.capacityRef.current;
     const batPowerDraw: HTMLDivElement = this.powerDrawRef.current;
-    const chargeNow = await this.getPower('energy_now');
-    const chargeFull = await this.getPower('energy_full');
-    const powerDraw = await this.getPower('power_now');
+    const chargeNow = await this.props.pt.getPower('energy_now');
+    const chargeFull = await this.props.pt.getPower('energy_full');
+    const powerDraw = await this.props.pt.getPower('power_now');
     batCapacityNow.innerText =
       ((7.7 * chargeNow) / 1000000).toFixed(2).toString() +
       ' Wh (' +
@@ -58,34 +44,38 @@ export class Battery extends Component<AppProps> {
 
   render(props: AppProps) {
     return (
-      <div class="quickaccesscontrols_PanelSectionRow_2VQ88">
-        <div
-          class="gamepaddialog_Field_S-_La gamepaddialog_WithFirstRow_qFXi6 gamepaddialog_VerticalAlignCenter_3XNvA gamepaddialog_InlineWrapShiftsChildrenBelow_pHUb6 gamepaddialog_WithBottomSeparator_1lUZx gamepaddialog_StandardPadding_XRBFu gamepaddialog_HighlightOnFocus_wE4V6 Panel Focusable"
-          style="--indent-level: 0"
-        >
-          <div class="gamepaddialog_FieldLabelRow_H9WOq">
-            <div class="gamepaddialog_FieldLabel_3b0U-">System Charge</div>
-            <div class="gamepaddialog_FieldChildren_14_HB">
-              <div
-                class="gamepaddialog_LabelFieldValue_5Mylh"
-                id="batCapacityNow"
-                ref={this.capacityRef}
-              ></div>
-            </div>
+      <div
+        class="quickaccesscontrols_PanelSectionRow_2VQ88"
+        style="padding: 0px 4px"
+      >
+        <div class="gamepaddialog_Field_S-_La gamepaddialog_WithFirstRow_qFXi6 gamepaddialog_WithChildrenBelow_1u5FT gamepaddialog_VerticalAlignCenter_3XNvA gamepaddialog_InlineWrapShiftsChildrenBelow_pHUb6 gamepaddialog_ChildrenWidthFixed_1ugIU gamepaddialog_ExtraPaddingOnChildrenBelow_5UO-_ gamepaddialog_StandardPadding_XRBFu gamepaddialog_HighlightOnFocus_wE4V6 Panel Focusable ">
+          <div class="quickaccesscontrols_PanelSectionTitle_2iFf9">
+            <div class="quickaccesscontrols_Text_1hJkB">Battery</div>
           </div>
-        </div>
-        <div
-          class="gamepaddialog_Field_S-_La gamepaddialog_WithFirstRow_qFXi6 gamepaddialog_VerticalAlignCenter_3XNvA gamepaddialog_InlineWrapShiftsChildrenBelow_pHUb6 gamepaddialog_WithBottomSeparator_1lUZx gamepaddialog_StandardPadding_XRBFu gamepaddialog_HighlightOnFocus_wE4V6 Panel Focusable"
-          style="--indent-level: 0"
-        >
-          <div class="gamepaddialog_FieldLabelRow_H9WOq">
-            <div class="gamepaddialog_FieldLabel_3b0U-">Battery Power Draw</div>
-            <div class="gamepaddialog_FieldChildren_14_HB">
-              <div
-                class="gamepaddialog_LabelFieldValue_5Mylh"
-                id="batPowerDraw"
-                ref={this.powerDrawRef}
-              ></div>
+          <div class="quickaccesscontrols_PanelSectionRow_2VQ88">
+            <div
+              class="gamepaddialog_Field_S-_La gamepaddialog_WithFirstRow_qFXi6 gamepaddialog_VerticalAlignCenter_3XNvA gamepaddialog_InlineWrapShiftsChildrenBelow_pHUb6 gamepaddialog_WithBottomSeparator_1lUZx gamepaddialog_StandardPadding_XRBFu gamepaddialog_HighlightOnFocus_wE4V6 Panel Focusable"
+              style="--indent-level: 0"
+            >
+              <div class="gamepaddialog_FieldLabelRow_H9WOq">
+                <div class="gamepaddialog_FieldLabel_3b0U-">Charge</div>
+                <div
+                  class="gamepaddialog_LabelFieldValue_5Mylh"
+                  ref={this.capacityRef}
+                />
+              </div>
+            </div>
+            <div
+              class="gamepaddialog_Field_S-_La gamepaddialog_WithFirstRow_qFXi6 gamepaddialog_VerticalAlignCenter_3XNvA gamepaddialog_InlineWrapShiftsChildrenBelow_pHUb6 gamepaddialog_WithBottomSeparator_1lUZx gamepaddialog_StandardPadding_XRBFu gamepaddialog_HighlightOnFocus_wE4V6 Panel Focusable"
+              style="--indent-level: 0"
+            >
+              <div class="gamepaddialog_FieldLabelRow_H9WOq">
+                <div class="gamepaddialog_FieldLabel_3b0U-">Power Draw</div>
+                <div
+                  class="gamepaddialog_LabelFieldValue_5Mylh"
+                  ref={this.powerDrawRef}
+                />
+              </div>
             </div>
           </div>
         </div>
--- src/components/SystemLabel.tsx
+++ src/components/SystemLabel.tsx
@@ -1,34 +1,42 @@
 import { Component, createRef } from 'preact';
-import { AppProps } from '../view';
+import { SMM } from '../types/SMM';
+import { PowerTools } from '../util';
 
-export class SystemLabel extends Component<AppProps> {
-  ref = createRef();
-  constructor(props: AppProps) {
-    super(props);
-  }
+export interface SystemLabelProps {
+  smm: SMM;
+  pt: PowerTools;
+}
 
-  // Gets the system id
-  async getSysID(): Promise<string> {
-    const id = await this.props.smm.FS.readFile(
-      '/sys/devices/virtual/dmi/id/product_name'
-    );
-    return id.trim();
-  }
+export class SystemLabel extends Component<SystemLabelProps> {
+  sysID = createRef();
+  cpuID = createRef();
 
   async componentDidMount() {
-    if (this.ref.current) {
-      const label: HTMLDivElement = this.ref.current;
-      label.innerText = await this.getSysID();
+    if (this.sysID.current) {
+      this.sysID.current.innerText = await this.props.pt.getSysID();
+      this.cpuID.current.innerText = await this.props.pt.getCPUID();
     }
   }
 
-  render(props: AppProps) {
+  render(props: SystemLabelProps) {
     return (
       <div
-        id="sysID_Lab"
-        class="quickaccesscontrols_Text_1hJkB"
-        ref={this.ref}
-      ></div>
+        class="quickaccesscontrols_PanelSectionRow_2VQ88"
+        style="padding: 0px 4px"
+      >
+        <div
+          class="gamepaddialog_Field_S-_La gamepaddialog_WithFirstRow_qFXi6 gamepaddialog_VerticalAlignCenter_3XNvA gamepaddialog_InlineWrapShiftsChildrenBelow_pHUb6 gamepaddialog_WithBottomSeparator_1lUZx gamepaddialog_StandardPadding_XRBFu gamepaddialog_HighlightOnFocus_wE4V6 Panel Focusable"
+          style="--indent-level: 0"
+        >
+          <div class="gamepaddialog_FieldLabelRow_H9WOq">
+            <div class="gamepaddialog_FieldLabel_3b0U-">
+              <div class="gamepadslider_LabelText_1-PvK" ref={this.sysID}></div>
+            </div>
+          </div>
+          <div class="gamepaddialog_FieldDescription_2OJfk" ref={this.cpuID} />
+        </div>
+      </div>
     );
   }
 }
+//style="padding: 0px 4px"
