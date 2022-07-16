7124a1f1901316ff0207321660f9346e16668fce
Derek J. Clark <derekjohn.clark@gmail.com>
2022-07-15 22:53:26 -0700

    Combine SystemLabel and VersionLabel into one component.

--- src/components/SystemLabel.tsx
+++ src/components/SystemLabel.tsx
@@ -8,13 +8,22 @@ export interface SystemLabelProps {
 }
 
 export class SystemLabel extends Component<SystemLabelProps> {
-  sysID = createRef();
   cpuID = createRef();
+  sysID = createRef();
+  versionID = createRef();
 
   async componentDidMount() {
+    if (this.cpuID.current) {
+      this.cpuID.current.innerText = await this.props.pt.getCPUID();
+    }
+
     if (this.sysID.current) {
       this.sysID.current.innerText = await this.props.pt.getSysID();
       this.cpuID.current.innerText = await this.props.pt.getCPUID();
+    }
+
+    if (this.versionID.current) {
+      this.versionID.current.innerText = await this.props.pt.getVersion();
     }
   }
 
@@ -24,19 +33,43 @@ export class SystemLabel extends Component<SystemLabelProps> {
         class="quickaccesscontrols_PanelSectionRow_2VQ88"
         style="padding: 0px 4px"
       >
-        <div
-          class="gamepaddialog_Field_S-_La gamepaddialog_WithFirstRow_qFXi6 gamepaddialog_VerticalAlignCenter_3XNvA gamepaddialog_InlineWrapShiftsChildrenBelow_pHUb6 gamepaddialog_WithBottomSeparator_1lUZx gamepaddialog_StandardPadding_XRBFu gamepaddialog_HighlightOnFocus_wE4V6 Panel Focusable"
-          style="--indent-level: 0"
-        >
-          <div class="gamepaddialog_FieldLabelRow_H9WOq">
-            <div class="gamepaddialog_FieldLabel_3b0U-">
-              <div class="gamepadslider_LabelText_1-PvK" ref={this.sysID}></div>
+        <div class="gamepaddialog_Field_S-_La gamepaddialog_WithFirstRow_qFXi6 gamepaddialog_WithChildrenBelow_1u5FT gamepaddialog_VerticalAlignCenter_3XNvA gamepaddialog_InlineWrapShiftsChildrenBelow_pHUb6 gamepaddialog_ChildrenWidthFixed_1ugIU gamepaddialog_ExtraPaddingOnChildrenBelow_5UO-_ gamepaddialog_StandardPadding_XRBFu gamepaddialog_HighlightOnFocus_wE4V6 Panel Focusable ">
+          <div class="quickaccesscontrols_PanelSectionTitle_2iFf9">
+            <div class="quickaccesscontrols_Text_1hJkB">System Info</div>
+          </div>
+          <div class="quickaccesscontrols_PanelSectionRow_2VQ88">
+            <div
+              class="gamepaddialog_Field_S-_La gamepaddialog_WithFirstRow_qFXi6 gamepaddialog_VerticalAlignCenter_3XNvA gamepaddialog_InlineWrapShiftsChildrenBelow_pHUb6 gamepaddialog_WithBottomSeparator_1lUZx gamepaddialog_StandardPadding_XRBFu gamepaddialog_HighlightOnFocus_wE4V6 Panel Focusable"
+              style="--indent-level: 0"
+            >
+              <div class="gamepaddialog_FieldLabelRow_H9WOq">
+                <div class="gamepaddialog_FieldLabel_3b0U-">
+                  <div
+                    class="quickaccesscontrols_Text_1hJkB"
+                    ref={this.sysID}
+                  />
+                </div>
+              </div>
+              <div
+                class="gamepaddialog_FieldDescription_2OJfk"
+                ref={this.cpuID}
+              />
+            </div>
+            <div
+              class="gamepaddialog_Field_S-_La gamepaddialog_WithFirstRow_qFXi6 gamepaddialog_VerticalAlignCenter_3XNvA gamepaddialog_InlineWrapShiftsChildrenBelow_pHUb6 gamepaddialog_WithBottomSeparator_1lUZx gamepaddialog_StandardPadding_XRBFu gamepaddialog_HighlightOnFocus_wE4V6 Panel Focusable"
+              style="--indent-level: 0"
+            >
+              <div class="gamepaddialog_FieldLabelRow_H9WOq">
+                <div class="gamepaddialog_FieldLabel_3b0U-">HandyPT</div>
+                <div
+                  class="gamepaddialog_LabelFieldValue_5Mylh"
+                  ref={this.versionID}
+                />
+              </div>
             </div>
           </div>
-          <div class="gamepaddialog_FieldDescription_2OJfk" ref={this.cpuID} />
         </div>
       </div>
     );
   }
 }
-//style="padding: 0px 4px"
