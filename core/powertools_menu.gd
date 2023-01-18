24010aea3ca8199285281b7ca319b0243e659400
Derek J. Clark <derekjohn.clark@gmail.com>
2023-01-17 21:39:32 -0800

    Polkit, value labels, cpupower basics

--- core/powertools_menu.gd
+++ core/powertools_menu.gd
@@ -1,18 +1,22 @@
 extends VBoxContainer
 
-const powertools_path : String = "/home/blip/git/github/ShadowBlip/HandyPT/etc/handypt/powertools"
+const powertools_path : String = "/etc/handypt/powertools"
 
 @onready var cpu_boost_button := $CPUBoostButton
 @onready var gpu_freq_enable := $GPUFreqEnableButton
 @onready var gpu_freq_max_slider := $GPUFreqMaxSlider
+@onready var gpu_freq_max_value_label := $GPUFreqMaxLabelContainer/GPUFreqMaxValueLabel
 @onready var gpu_freq_min_slider := $GPUFreqMinSlider
+@onready var gpu_freq_min_value_label := $GPUFreqMinLabelContainer/GPUFreqMinValueLabel
 @onready var smt_button := $SMTButton
 @onready var tdp_boost_slider := $TDPBoostSlider
+@onready var tdp_boost_value_label := $TDPBoostLabelContainer/TDPBoostValueLabel
 @onready var tdp_slider := $TDPSlider
-
+@onready var tdp_value_label := $TDPLabelContainer/TDPValueLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Find defaults and current settings.
 	if read_sys("/sys/devices/system/cpu/cpufreq/boost") == "1":
 		cpu_boost_button.button_pressed = true
 	if read_sys("/sys/class/drm/card0/device/power_dpm_force_performance_level") == "manual":
@@ -21,7 +25,8 @@ func _ready():
 		gpu_freq_min_slider.editable = true
 	if read_sys("/sys/devices/system/cpu/smt/control") == "on":
 		smt_button.button_pressed = true
-	
+
+	# Connect Signals
 	cpu_boost_button.toggled.connect(_on_toggle_cpu_boost)
 	gpu_freq_enable.toggled.connect(_on_toggle_gpu_freq)
 	gpu_freq_max_slider.value_changed.connect(_on_max_gpu_freq_changed)
@@ -29,7 +34,12 @@ func _ready():
 	smt_button.toggled.connect(_on_toggle_smt)
 	tdp_boost_slider.value_changed.connect(_on_tdp_boost_value_changed)
 	tdp_slider.value_changed.connect(_on_tdp_value_changed)
-	
+
+	# Set up UI with current values
+	gpu_freq_max_value_label.text = str(gpu_freq_max_slider.value)
+	gpu_freq_min_value_label.text = str(gpu_freq_min_slider.value)
+	tdp_boost_value_label.text = str(tdp_boost_slider.value)
+	tdp_value_label.text = str(tdp_slider.value)
 	
 	# Set write mode for power_dpm_force_performance_level
 	var output = []
@@ -42,26 +52,31 @@ func _process(delta):
 
 
 func read_sys(path: String) -> String:
-	print("Attempting to read " + path)
 	var output = []
 	var exit_code := OS.execute("cat", [path], output)
-	print(output[0].strip_escapes())
 	return output[0].strip_escapes()
 
+
+func _on_get_current_apu_settings() -> bool:
+	var output = []
+	var exit_code := OS.execute(powertools_path, ["ryzenadj", "-i"], output)
+	if exit_code:
+		return false
+	return true	
 
 func _on_max_gpu_freq_changed(value: float):
 	if value < gpu_freq_min_slider.value:
 		gpu_freq_min_slider.value = value
-	print("gpu_max")
+	gpu_freq_max_value_label.text = str(value)
 
 
 func _on_min_gpu_freq_changed(value: float):
 	if value > gpu_freq_max_slider.value:
 		gpu_freq_max_slider.value = value
-	print("gpu_min")
 	var output = []
 	var args = ["gpuclk", "0", str(value)]
 	var exit_code := OS.execute(powertools_path, args, output)
+	gpu_freq_min_value_label.text = str(value)
 
 
 func _on_tdp_boost_value_changed(value: float):
@@ -70,12 +85,14 @@ func _on_tdp_boost_value_changed(value: float):
 	var output = []
 	var exit_code := OS.execute(powertools_path, ["ryzenadj", "-c", str(fastPPT)], output)
 	exit_code = OS.execute(powertools_path, ["ryzenadj", "-a", str(slowPPT)], output)
+	tdp_boost_value_label.text = str(value)
 
 
 func _on_tdp_value_changed(value: float):
 	var output = []
 	var exit_code := OS.execute(powertools_path, ["ryzenadj", "-b", str(value)], output)
 	_on_tdp_boost_value_changed(tdp_boost_slider.value)
+	tdp_value_label.text = str(value)
 
 
 func _on_toggle_cpu_boost(state: bool):
@@ -104,6 +121,3 @@ func _on_toggle_smt(state: bool):
 		args = ["smt", "on"]
 	var output = []
 	var exit_code := OS.execute(powertools_path, args, output)
-
-
-
